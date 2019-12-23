822895c33870789600fa23b77235956dfc1269c3
Robin Haider <robinhaider69@gmail.com>
2019-12-24 00:53:36 +0600

    Day Three - Master Asp.net Core 3 - 3. Spice app - Category40%

---
+++ Spice/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Get all Categories....
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Categories.ToListAsync());
+        }
+
+        //Get Create..
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
