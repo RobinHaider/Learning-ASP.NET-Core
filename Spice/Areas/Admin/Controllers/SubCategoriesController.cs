b3a50b9fe36e1c9e224eafd008145afd81226d24
Robin Haider <robinhaider69@gmail.com>
2019-12-28 17:01:25 +0600

    Day Six - Master Asp.net Core 3 - 5. Spice app - SubCategory30%

---
+++ Spice/Areas/Admin/Controllers/SubCategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModels;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = await _db.SubCategories.Include(s=>s.Category).ToListAsync();
+            return View(subCategories);
+        }
+
+        //Get - Create
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Categories.ToListAsync(),
+                SubCategory = new SubCategory(),
+                SubCategoryList = await _db.SubCategories.OrderBy(p=>p.Name).Select(p=>p.Name).Distinct().ToListAsync()
+            };
+            return View(model);
+        }
+    }
+}
