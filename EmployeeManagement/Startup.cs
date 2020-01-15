c782fbbb11f9728647a1515f38604c0c548d29c7
Robin Haider <robinhaider69@gmail.com>
2020-01-15 23:22:19 +0600

    Kudvenkat - Asp.net core for beginners - Day Seven - 10 -global exception, logging, loglevel, Identity, user manager, signin manager, authorization

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -6,6 +6,7 @@ using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,8 @@ namespace EmployeeManagement
         {
             services.AddDbContextPool<AppDbContext>(
                 options=> options.UseSqlServer(_config.GetConnectionString("EmployeeDbConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
             services.AddMvc();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
@@ -44,7 +47,8 @@ namespace EmployeeManagement
             }
             
             app.UseStaticFiles();
-            //app.UseMvcWithDefaultRoute();
+
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
